65bde652d9f8e0821d1c6aa20e1ffe2f154ff55e
hums.k <humaira.khoirunnisa96@gmail.com>
2020-09-08 13:34:43 +0700

    bisa get biodata&person by nik

--- src/main/resources/static/assets/js/biodata.js
+++ src/main/resources/static/assets/js/biodata.js
@@ -59,10 +59,11 @@ var formBiodata = {
     resetForm: function () {
         $('#formprofile')[0].reset();
     },
+
     saveForm: function () {
 
         var dataResult = getJsonForm($("#formprofile").serializeArray(), true);
-		
+
         $.ajax({
             url: '/person',
             method: 'post',
@@ -70,9 +71,10 @@ var formBiodata = {
             dataType: 'json',
             data: JSON.stringify(dataResult),
             success: function (result) {
+                console.log(result);
                 $('#modal-biodata').modal('hide')
 
-                if (result.status == 'true' ) {
+                if (result.status == 'true') {
                     Swal.fire({
                         position: 'top-end',
                         icon: 'success',
@@ -86,31 +88,103 @@ var formBiodata = {
                         icon: 'error',
                         title: 'Your data is failed to be saved' + result.message,
                         showConfirmButton: false,
-                        timer: 1000
+                        timer: 3000
                     })
                 }
             },
             error: function (err) {
                 console.log(err);
             }
-        });
 
-    }, setEditData: function (idCabang) {
-        formBiodata.resetForm();
+        }
+        );
+
+
+    }, getBiodata: function (nik) {
+        //formBiodata.resetForm();
+
+        if ($.fn.DataTable.isDataTable('#mytable')) {
+            //table yg sudah dibentuk menjadi datatable harus d rebuild lagi untuk di instantiasi ulang
+            $('#mytable').DataTable().clear();
+            $('#mytable').DataTable().destroy();
+        }
 
         $.ajax({
-            url: '/person' + idCabang,
+            url: '/person/' + nik,
             method: 'get',
             contentType: 'application/json',
             dataType: 'json',
-            success: function (res, status, xhr) {
 
+            success: function (result) {
+                //var hasil = JSON.stringify(result),
+                console.log(result[0]);
+
+                // var coba = JSON.stringify(result);
+                // console.log(coba);
+                // console.log(coba.message);
+
+                if (result[0].status == 'true') {
+                    $('#mytable').DataTable({
+                        "lengthChange": false,
+                        "searching": false,
+                        "autoWidth": false,
+                        "responsive": false,
+                        "bPaginate": false,
+                        "bSort": false,
+                        "bInfo": false,
+                        data: [result[0].data],
+                        columns: [
+                            {
+                                title: "Nik",
+                                data: "nik"
+                            },
+                            {
+                                title: "Nama",
+                                data: "name"
+                            },
+                            {
+                                title: "Alamat",
+                                data: "adress"
+                            },
+                            {
+                                title: "Nomor hp",
+                                data: "hp"
+                            },
+                            {
+                                title: "Tanggal Lahir",
+                                data: "tgl"
+                            },
+                            {
+                                title: "Tempat Lahir",
+                                data: "tmptLahir"
+                            },
+                            {
+                                title: "Umur",
+                                data: "umur"
+                            },
+                            {
+                                title: "Pendidikan Terakhir",
+                                data: "pddknTerakhir"
+                            }
+                        ]
+                    });
+
+                } else {
+
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'error',
+                        title: result.message,
+                        showConfirmButton: false,
+                        timer: 1000
+                    })
+
+                }
             },
             error: function (err) {
                 console.log(err);
             }
         });
-
 
     }
 
